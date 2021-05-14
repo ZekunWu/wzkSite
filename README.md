919a5481d33ce4cfa93a45621d21a89a17e1b195
sin2 <1430755343@qq.com>
2021-05-14 17:17:44 +0800

    添加node.config.js，发布版本一步化

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 - 代码基于ice.js + ts
 - 后端基于egg.js满足简单接口需求
+- 添加node.config.js脚本，发布新版本流程一步化
---
+++ node.config.js
@@ -0,0 +1,26 @@
+const shell = require('shelljs')
+const packageJson = require('./package.json')
+
+new Promise((res, rej) => {
+  /* 提示编译成功 */
+  shell.echo('编译成功')
+
+  /* 执行提交更新与推送 */
+  if (!shell.which('git')) {
+    rej(new Error('您当前的运行环境中缺少 Git'))
+  } else if (
+    !shell.exec(`git add . && git commit -m "build: ${packageJson.version} 版本" && git tag "${packageJson.version}"`)
+      .code
+  ) {
+    if (!shell.exec(`git push && git push origin "${packageJson.version}"`).code) {
+      shell.echo('成功发布版本')
+      res()
+    } else {
+      rej(new Error('推送版本失败'))
+    }
+  } else {
+    rej(new Error('提交改动文件失败'))
+  }
+}).catch((error) => {
+  throw error
+})
