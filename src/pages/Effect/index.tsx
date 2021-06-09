60ae35979174916b92521bf19d7ca92cca5e40c2
sin2 <1430755343@qq.com>
2021-06-09 11:43:34 +0800

    feat: 添加动态变色字体特效

--- src/pages/Effect/index.tsx
+++ src/pages/Effect/index.tsx
@@ -10,6 +10,10 @@ function Effect() {
       <BiliBanner />
       <div className={styles.header}>实现长列表无限滚动：</div>
       <UnlimitedList listArr={Array(10000).fill(0).map((item, index) => index + 1)} size={5} bufferSize={2} itemHeight={40} />
+      <div className={styles.header}>实现动态变色字体：</div>
+      <div className={styles.colorfulArea}>
+        <div className={styles.colorful}>生命中的美好光芒，靠的都是坚持</div>
+      </div>
     </div>
   );
 }
